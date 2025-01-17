95c1e5ca170702eaaa937dc21d503358dd3222bd
Marcelo <marcelo.s44@gmail.com>
2025-01-16 20:37:13 -0500

    Config project CSS Variables

--- 02 - JS and CSS Clock/js/app.js
+++ 02 - JS and CSS Clock/js/app.js
@@ -1,6 +1,6 @@
-const secondHand = document.querySelector('.second-hand')
-const minsHand = document.querySelector('.min-hand')
-const hourHand = document.querySelector('.hour-hand')
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
@@ -13,14 +13,11 @@ function setDate() {
     const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-
-
 }
 
 setInterval(setDate, 1000);
+
 setDate();
